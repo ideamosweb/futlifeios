d5ed7003f59f1bd7d4647849db2f4dfda97a2bfb
Rene Santis <rene.santis@globant.com>
2016-10-16 11:56:07 -0500

    Initial Commit

---
+++ FutLife/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FutLife
+//
+//  Created by Rene Santis on 10/16/16.
+//  Copyright © 2016 IdeamosWeb S.A.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
