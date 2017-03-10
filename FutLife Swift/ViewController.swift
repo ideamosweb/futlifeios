8fa40491850278db7292e66c37146702e4060300
Rene Santis <rene.santis@globant.com>
2017-03-10 15:42:56 -0500

    Initial Commit

---
+++ FutLife Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FutLife Swift
+//
+//  Created by Rene Santis on 3/10/17.
+//  Copyright © 2017 IdeamosWeb S.A.S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
