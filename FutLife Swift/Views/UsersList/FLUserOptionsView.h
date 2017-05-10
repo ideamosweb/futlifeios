957429c3db96eeacf74a25523a7e7bee6d1b767b
Rene Santis <rene.santis@globant.com>
2017-05-09 23:13:24 -0500

    added view files

---
+++ FutLife Swift/Views/UsersList/FLUserOptionsView.h
@@ -0,0 +1,31 @@
+//
+//  FLUserOptionsView.h
+//  FutLife
+//
+//  Created by Rene Santis on 2/3/17.
+//  Copyright © 2017 IdeamosWeb S.A.S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FLUserModel.h"
+
+@protocol FLUserOptionsProtocol <NSObject>
+
+- (void)goToViewProfileWithUser:(FLUserModel *)userModel avatar:(UIImage *)avatar userName:(NSString *)userName name:(NSString *)name;
+- (void)goToChallengeWithUserOpponent:(FLUserModel *)userOpponent;
+- (void)goToViewConsolesWithUser:(FLUserModel *)userModel;
+- (void)goToViewGamesWithUser:(FLUserModel *)userModel;
+
+@end
+
+@interface FLUserOptionsView : UIView
+
+@property (weak, nonatomic) IBOutlet UILabel *userOptionsNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *userOptionsUserNameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *userOptionsAvatarImageView;
+
+@property (strong, nonatomic) FLUserModel *user;
+
+@property (assign, nonatomic) id<FLUserOptionsProtocol> delegate;
+
+@end
